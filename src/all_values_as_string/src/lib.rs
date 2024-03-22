ede42a5b6ab019834c94fbc77b68cfe6b75ce1fa
fredyakumo <fredyakumo@gmail.com>
2024-03-23 01:55:30 +0800

    init cargo

---
+++ src/all_values_as_string/src/lib.rs
@@ -0,0 +1,110 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+
+#[proc_macro_derive(AllFieldNamesAsString, attributes(StructFieldAsString))]
+pub fn all_field_name_as_str_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = &input.ident;
+    let fields =
+        if let syn::Data::Struct(
+            syn::DataStruct {
+                fields: syn::Fields::Named(
+                    syn::FieldsNamed { named, .. }), ..
+            }) = input.data
+        {
+            named
+        } else {
+            panic!("Only works on structs with named fields")
+        };
+
+        let field_parts = fields.iter().map(|f| {
+            let ident = &f.ident;
+            if f.attrs.iter().any(|attr| attr.path().is_ident("StructFieldAsString")) {
+                // Field has the `#[helper]` attribute, assume it implements the trait
+                quote! {
+                    let field_str = <_ as AllFieldNamesAsString>::get_all_field_names_as_string(&self.#ident);
+                    field_parts.push(field_str);
+                }
+            } else {
+                // Field does not have the `#[helper]` attribute, use the field name as string
+                quote! {
+                    let field_str = stringify!(#ident).to_string();
+                    field_parts.push(field_str);
+                }
+            }
+        });
+
+    let expanded = quote! {
+        impl AllFieldNamesAsString for #name {
+            fn get_all_field_names_as_string(&self) -> String {
+                let mut field_parts: Vec<String> = Vec::new();
+                #(#field_parts)*
+                field_parts.join(", ")
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+#[proc_macro_derive(AllValuesAsString, attributes(StructFieldAsString, NumericField, BooleanField))]
+pub fn all_values_as_str_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = &input.ident;
+    let fields =
+        if let syn::Data::Struct(
+            syn::DataStruct {
+                fields: syn::Fields::Named(
+                    syn::FieldsNamed { named, .. }), ..
+            }) = input.data
+        {
+            named
+        } else {
+            panic!("Only works on structs with named fields")
+        };
+
+        let field_parts = fields.iter().map(|f| {
+            let ident = &f.ident;
+            if f.attrs.iter().any(|attr| attr.path().is_ident("StructFieldAsString")) {
+                // Field has the `#[helper]` attribute, assume it implements the trait
+                quote! {
+                    let field_str = <_ as AllValuesAsString>::get_all_values_as_string(&self.#ident);
+                    field_parts.push(field_str);
+                }
+            } else if f.attrs.iter().any(|attr| attr.path().is_ident("NumericField")) {
+                quote! {
+                    let field_str = format!("{}", &self.#ident);
+                    field_parts.push(field_str);
+                }
+            } else if f.attrs.iter().any(|attr| attr.path().is_ident("BooleanField")) {
+                quote! {
+                    let field_str = if self.#ident { "1".into() } else { "0".into() };
+                    field_parts.push(field_str);
+                }
+            }else {
+                // Field does not have the `#[helper]` attribute, use the field name as string
+                quote! {
+                    let field_str = format!("'{}'", &self.#ident);
+                    field_parts.push(field_str);
+                }
+            }
+        });
+
+    let expanded = quote! {
+        impl AllValuesAsString for #name {
+            fn get_all_values_as_string(&self) -> String {
+                let mut field_parts: Vec<String> = Vec::new();
+                #(#field_parts)*
+                field_parts.join(", ")
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use all_values_as_string::{AllFieldNamesAsString, AllValuesAsString};
+use chrono::{NaiveDateTime, Utc};
+
+pub trait AllFieldNamesAsString {
+    fn get_all_field_names_as_string(&self) -> String;
+}
+
+pub trait AllValuesAsString {
+    fn get_all_values_as_string(&self) -> String;
+}
+
+#[derive(AllFieldNamesAsString, AllValuesAsString)]
+pub struct Foo {
+    #[NumericField]
+    a: f64,
+    b: String,
+}
+
+#[derive(AllFieldNamesAsString, AllValuesAsString)]
+pub struct PageTableEntry {
+    #[StructFieldAsString]
+    f: Foo,
+    #[BooleanField]
+    valid: bool,
+    #[BooleanField]
+    read: bool,
+    #[BooleanField]
+    write: bool,
+    #[BooleanField]
+    executable: bool,
+    #[BooleanField]
+    user: bool,
+    #[BooleanField]
+    global: bool,
+    #[BooleanField]
+    accessed: bool,
+    #[BooleanField]
+    dirty: bool,
+    #[NumericField]
+    rsvd1: usize,
+    #[NumericField]
+    ppn: usize,
+    #[NumericField]
+    rsvd2: usize,
+    time: NaiveDateTime
+}
+
+fn main() {
+    let a= PageTableEntry{
+        f: Foo {
+            a: 1000.,
+            b: "test".into(),
+        },
+        valid: false,
+        read: false,
+        write: false,
+        executable: false,
+        user: false,
+        global: false,
+        accessed: false,
+        dirty: false,
+        rsvd1: 0,
+        ppn: 0,
+        rsvd2: 0,
+        time: Utc::now().naive_local(),
+    };
+    println!("{}", a.get_all_field_names_as_string());
+    println!("{}", a.get_all_values_as_string());
+}
