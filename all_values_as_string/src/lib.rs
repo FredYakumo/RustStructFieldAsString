3afcbf596274af763e84e101a395f79caa4f8990
fredyakumo <fredyakumo@gmail.com>
2024-08-02 01:01:02 +0800

    Change return value from string to vec[string]

--- all_values_as_string/src/lib.rs
+++ all_values_as_string/src/lib.rs
@@ -26,7 +26,7 @@ pub fn all_field_name_as_str_derive(input: TokenStream) -> TokenStream {
             if f.attrs.iter().any(|attr| attr.path().is_ident("StructFieldAsString")) {
                 // Field has the `#[helper]` attribute, assume it implements the trait
                 quote! {
-                    field_parts.push(self.#ident.get_all_field_names_as_string());
+                    field_parts.extend(self.#ident.get_all_field_names_as_string());
                 }
             } else {
                 // Field does not have the `#[helper]` attribute, use the field name as string
@@ -39,10 +39,10 @@ pub fn all_field_name_as_str_derive(input: TokenStream) -> TokenStream {
 
     let expanded = quote! {
         impl #name {
-            fn get_all_field_names_as_string(&self) -> String {
+            pub fn get_all_field_names_as_string(&self) -> Vec<String> {
                 let mut field_parts: Vec<String> = Vec::new();
                 #(#field_parts)*
-                field_parts.join(", ")
+                field_parts
             }
         }
     };
@@ -50,7 +50,7 @@ pub fn all_field_name_as_str_derive(input: TokenStream) -> TokenStream {
     TokenStream::from(expanded)
 }
 
-#[proc_macro_derive(AllValuesAsString, attributes(StructFieldAsString, NumericField, BooleanField))]
+#[proc_macro_derive(AllValuesAsString, attributes(StructFieldAsString, NumericField, BooleanField, OptionField, OptionNumericField))]
 pub fn all_values_as_str_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -72,33 +72,43 @@ pub fn all_values_as_str_derive(input: TokenStream) -> TokenStream {
             if f.attrs.iter().any(|attr| attr.path().is_ident("StructFieldAsString")) {
                 // Field has the `#[helper]` attribute, assume it implements the trait
                 quote! {
-                    field_parts.push(self.#ident.get_all_values_as_string());
+                    field_parts.extend(self.#ident.get_all_values_as_string());
                 }
             } else if f.attrs.iter().any(|attr| attr.path().is_ident("NumericField")) {
                 quote! {
                     let field_str = format!("{}", &self.#ident);
-                    field_parts.push(field_str);
+                    field_parts.push(if field_str == "NaN" { "0".into() } else { field_str } );
                 }
             } else if f.attrs.iter().any(|attr| attr.path().is_ident("BooleanField")) {
                 quote! {
                     let field_str = if self.#ident { "1".into() } else { "0".into() };
                     field_parts.push(field_str);
                 }
-            }else {
+            } else if f.attrs.iter().any(|attr| attr.path().is_ident("OptionNumericField")) {
+                quote! {
+                    let field_str = match self.#ident { Some(v) => format!("{}", v.to_string()), None => "null".into() };
+                    field_parts.push(field_str);
+                }
+            } else if f.attrs.iter().any(|attr| attr.path().is_ident("OptionField")) {
+                quote! {
+                    let field_str = match self.#ident { Some(v) => format!("'{}'", v.to_string()), None => "null".into() };
+                    field_parts.push(field_str);
+                }
+            } else {
                 // Field does not have the `#[helper]` attribute, use the field name as string
                 quote! {
-                    let field_str = format!("'{}'", &self.#ident);
-                    field_parts.push(field_str);
+                    let field_str = format!("{}", &self.#ident);
+                    field_parts.push(if field_str.len() < 1 { String::from("null") } else { format!("'{}'", field_str.replace('\'', "\\'") ) });
                 }
             }
         });
 
     let expanded = quote! {
         impl #name {
-            fn get_all_values_as_string(&self) -> String {
+            pub fn get_all_values_as_string(&self) -> Vec<String> {
                 let mut field_parts: Vec<String> = Vec::new();
                 #(#field_parts)*
-                field_parts.join(", ")
+                field_parts
             }
         }
     };
